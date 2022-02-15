df76f3dc49674763a54bff3cc4d279d98d58a1b6
CakeZ31 <51308530+CakeZ31@users.noreply.github.com>
2022-02-14 23:17:15 -0800

    done

--- app/src/main/java/com/example/flixsters/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixsters/adapters/MovieAdapter.java
@@ -1,21 +1,25 @@
 package com.example.flixsters.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
-import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixsters.DetailActivity;
 import com.example.flixsters.R;
 import com.example.flixsters.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -53,7 +57,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -63,6 +67,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movie) {
@@ -78,6 +83,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             }
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+            // 1. Register click listener on the whole row
+            // 2. Navigate to a new activity on tap
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Intent i = new Intent(context, DetailActivity.class);
+                   i.putExtra("movie", Parcels.wrap(movie));
+                   context.startActivity(i);
+                }
+            });
         }
     }
 
